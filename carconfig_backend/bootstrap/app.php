1c8f0a362fc4221436b248062ee44d674404474b
Taranta98 <marcotarantelli98@outlook.it>
2026-06-24 12:10:20 +0200

    modifiche per lo storage, fix bug deploy per img

--- carconfig_backend/bootstrap/app.php
+++ carconfig_backend/bootstrap/app.php
@@ -8,7 +8,6 @@ use Illuminate\Foundation\Configuration\Middleware;
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
         api: __DIR__.'/../routes/api.php',
-        web: __DIR__.'/../routes/web.php',
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware): void {
